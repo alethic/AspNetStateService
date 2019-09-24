35dba34b213a69f61cf6dcff5eeb0d553818dc68
Jerome Haltom <jhaltom@fileandserve.com>
2019-09-24 15:01:45 -0500

    Upgrade to V2 remoting protocol, properly, for actors.

--- AspNetStateService.Fabric.Interfaces/Properties/AssemblyInfo.cs
+++ AspNetStateService.Fabric.Interfaces/Properties/AssemblyInfo.cs
@@ -2,8 +2,10 @@
 
 using Castle.Core.Internal;
 
+using Microsoft.ServiceFabric.Actors.Remoting.FabricTransport;
 using Microsoft.ServiceFabric.Services.Remoting;
 using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
 
 [assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)]
 [assembly: FabricTransportServiceRemotingProvider(RemotingListenerVersion = RemotingListenerVersion.V2, RemotingClientVersion = RemotingClientVersion.V2)]
+[assembly: FabricTransportActorRemotingProvider(RemotingListenerVersion = RemotingListenerVersion.V2, RemotingClientVersion = RemotingClientVersion.V2)]
