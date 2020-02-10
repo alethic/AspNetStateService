2f136bdbc8f0ecb2850b27a2e0f563a731dda7c4
Jerome Haltom <wasabi@cogito.cx>
2020-02-09 21:32:00 -0600

    Allow configuration of database. Batch dual commands.

--- AspNetStateService.Redis/StateObjectRedisDataStore.cs
+++ AspNetStateService.Redis/StateObjectRedisDataStore.cs
@@ -28,6 +28,7 @@ namespace AspNetStateService.Redis
         readonly ILogger logger;
 
         IConnectionMultiplexer connection;
+        IDatabase database;
 
         /// <summary>
         /// Initializes a new instance.
@@ -52,14 +53,14 @@ namespace AspNetStateService.Redis
             logger.Verbose("InitAsync()");
 
             connection = await connections.GetConnectionAsync();
+            database = connection.GetDatabase(options.Value.DatabaseId ?? -1);
         }
 
         public async Task<(byte[] data, uint? extraFlags, TimeSpan? timeout, DateTime? altered)> GetDataAsync(string id, CancellationToken cancellationToken)
         {
             logger.Verbose("GetDataAsync({Id})", id);
 
-            var db = connection.GetDatabase();
-            var hs = await db.HashGetAsync(id, new RedisValue[] { "Data", "ExtraFlags", "Timeout", "Altered" });
+            var hs = await database.HashGetAsync(id, new RedisValue[] { "Data", "ExtraFlags", "Timeout", "Altered" });
             return (hs[0], (uint?)hs[1], !hs[2].IsNullOrEmpty ? (TimeSpan?)TimeSpan.FromTicks((long)hs[2]) : null, !hs[3].IsNullOrEmpty ? (DateTime?)new DateTime((long)hs[3]) : null);
         }
 
@@ -67,8 +68,7 @@ namespace AspNetStateService.Redis
         {
             logger.Verbose("GetLockAsync({Id})", id);
 
-            var db = connection.GetDatabase();
-            var hs = await db.HashGetAsync(id, new RedisValue[] { "LockCookie", "LockTime" });
+            var hs = await database.HashGetAsync(id, new RedisValue[] { "LockCookie", "LockTime" });
             return ((uint?)hs[0], !hs[1].IsNullOrEmpty ? (DateTime?)new DateTime((long)hs[1]) : null);
         }
 
@@ -76,32 +76,35 @@ namespace AspNetStateService.Redis
         {
             logger.Verbose("RemoveDataAsync({Id})", id);
 
-            var db = connection.GetDatabase();
-            await db.HashDeleteAsync(id, new RedisValue[] { "Data", "ExtraFlags", "Timeout", "Altered" });
+            var batch = database.CreateBatch();
+            var t1 = database.HashDeleteAsync(id, new RedisValue[] { "Data", "ExtraFlags", "Timeout", "Altered" });
+            var t2 = database.KeyExpireAsync(id, TimeSpan.FromMinutes(1)); batch.Execute();
+            await Task.WhenAll(t1, t2);
         }
 
         public async Task RemoveLockAsync(string id, CancellationToken cancellationToken)
         {
             logger.Verbose("RemoveLockAsync({Id})", id);
 
-            var db = connection.GetDatabase();
-            await db.HashDeleteAsync(id, new RedisValue[] { "LockCookie", "LockTime" });
+            await database.HashDeleteAsync(id, new RedisValue[] { "LockCookie", "LockTime" });
         }
 
         public async Task SetDataAsync(string id, byte[] data, uint? extraFlags, TimeSpan? timeout, CancellationToken cancellationToken)
         {
             logger.Verbose("SetDataAsync({Id}, {ExtraFlags}, {Timeout})", id, extraFlags, timeout);
 
-            var db = connection.GetDatabase();
-            await db.HashSetAsync(id, new HashEntry[] { new HashEntry("Data", data), new HashEntry("ExtraFlags", extraFlags), new HashEntry("Timeout", timeout?.Ticks), new HashEntry("Altered", DateTime.UtcNow.Ticks) });
+            var batch = database.CreateBatch();
+            var t1 = batch.HashSetAsync(id, new HashEntry[] { new HashEntry("Data", data), new HashEntry("ExtraFlags", extraFlags), new HashEntry("Timeout", timeout?.Ticks), new HashEntry("Altered", DateTime.UtcNow.Ticks) });
+            var t2 = batch.KeyExpireAsync(id, timeout + TimeSpan.FromMinutes(1) ?? TimeSpan.FromMinutes(20));
+            batch.Execute();
+            await Task.WhenAll(t1, t2);
         }
 
         public async Task SetFlagAsync(string id, uint? extraFlags, CancellationToken cancellationToken)
         {
             logger.Verbose("SetFlagAsync({Id}, {ExtraFlags})", id, extraFlags);
 
-            var db = connection.GetDatabase();
-            await db.HashSetAsync(id, new HashEntry[] { new HashEntry("ExtraFlags", extraFlags) });
+            await database.HashSetAsync(id, new HashEntry[] { new HashEntry("ExtraFlags", extraFlags) });
 
         }
 
@@ -109,16 +112,18 @@ namespace AspNetStateService.Redis
         {
             logger.Verbose("SetLockAsync({Id}, {Cookie}, {Time})", id, cookie, time);
 
-            var db = connection.GetDatabase();
-            await db.HashSetAsync(id, new HashEntry[] { new HashEntry("LockCookie", cookie), new HashEntry("LockTime", time.Ticks) });
+            await database.HashSetAsync(id, new HashEntry[] { new HashEntry("LockCookie", cookie), new HashEntry("LockTime", time.Ticks) });
         }
 
         public async Task SetTimeoutAsync(string id, TimeSpan? timeout, CancellationToken cancellationToken)
         {
             logger.Verbose("SetTimeoutAsync({Id}, {Timeout})", id, timeout);
 
-            var db = connection.GetDatabase();
-            await db.HashSetAsync(id, new HashEntry[] { new HashEntry("Timeout", timeout?.Ticks) });
+            var batch = database.CreateBatch();
+            var t1 = database.HashSetAsync(id, new HashEntry[] { new HashEntry("Timeout", timeout?.Ticks) });
+            var t2 = database.KeyExpireAsync(id, timeout + TimeSpan.FromMinutes(1) ?? TimeSpan.FromMinutes(20));
+            batch.Execute();
+            await Task.WhenAll(t1, t2);
         }
 
     }
--- AspNetStateService.Redis/StateObjectRedisDataStoreOptions.cs
+++ AspNetStateService.Redis/StateObjectRedisDataStoreOptions.cs
@@ -7,7 +7,15 @@ namespace AspNetStateService.Redis
     public class StateObjectRedisDataStoreOptions
     {
 
+        /// <summary>
+        /// Gets the StackExchange Redis configuration string.
+        /// </summary>
         public string Configuration { get; set; }
+
+        /// <summary>
+        /// Gets the database ID to connect to.
+        /// </summary>
+        public int? DatabaseId { get; set; }
 
     }
 
